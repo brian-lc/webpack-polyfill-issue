ab1aea59b15e38e2416d21cbef844b8f5030d850
Brian Chamberlain <brian.chamberlain@mesh.xyz>
2021-04-10 16:04:47 -0400

    functions update

--- src/components/HolderMedia.jsx
+++ src/components/HolderMedia.jsx
@@ -12,8 +12,24 @@ class HolderMedia extends React.Component {
  }
 
  componentDidMount() {
-    // Setup wallet connect objects 
-    this.setState({ media: [] });
+    // Setup wallet connect objects
+    const { userAccount } = this.state;
+    const NETLIFY_FUNC =
+      'priceless-mayer-402b9f.netlify.app/.netlify/functions';
+
+     fetch(`https://${NETLIFY_FUNC}/check-ownership?address=${userAccount}`
+      )
+      .then(x => x.json())
+      .then(x => {
+        console.log('HERE', x);
+      })
+    
+    const media = {
+        raw_media_path: 'https://filesamples.com/samples/audio/wav/Symphony%20No.6%20(1st%20movement).wav',
+        preview_media_path: 'https://filesamples.com/samples/audio/mp3/Symphony%20No.6%20(1st%20movement).mp3'
+    };
+
+    this.setState({ media: media });
  }
 
  render() {
--- src/lib/MediaAccessManager.js
+++ src/lib/MediaAccessManager.js
@@ -1,11 +1,10 @@
 class MediaAccessManager {
 
-  
-
   static requestAccessFor(userAddress) {
     const NETLIFY_FUNC =
       'priceless-mayer-402b9f.netlify.app.netlify.com/.netlify/functions';
-      fetch(
+
+     fetch(
         `https://${NETLIFY_FUNC}/check-ownership?address=${userAddress}`
       )
         .then(x => x.json())
@@ -14,10 +13,8 @@ class MediaAccessManager {
         })
     
     const mediaObjects = {
-      media: {
-          raw_media_path: 'https://filesamples.com/samples/audio/wav/Symphony%20No.6%20(1st%20movement).wav',
-          preview_media_path: 'https://filesamples.com/samples/audio/mp3/Symphony%20No.6%20(1st%20movement).mp3'
-      }
+        raw_media_path: 'https://filesamples.com/samples/audio/wav/Symphony%20No.6%20(1st%20movement).wav',
+        preview_media_path: 'https://filesamples.com/samples/audio/mp3/Symphony%20No.6%20(1st%20movement).mp3'
     };
 
     return (mediaObjects);
